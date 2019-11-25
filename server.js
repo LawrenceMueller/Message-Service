640e96b88c6311716d9eba5cb861e01c3462c43a
LAN_scape <lawrencemueller18@gmail.com>
2019-11-25 14:10:19 -0800

    The message bug has returned so its time to change the time the messages send so that I can test it

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === 'production') {
 */
 
 //Send SMS to everyone in the database
-cron.schedule('1 14 * * * ', function() {
+cron.schedule('15 14 * * * ', function() {
     let currentTextLocation = 0;
     let currentTextBody = '';
 
