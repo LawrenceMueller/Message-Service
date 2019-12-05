e0757037569f8e10e542afa9bc17dfee1942d837
LAN_scape <lawrencemueller18@gmail.com>
2019-12-04 23:10:32 -0800

    Changing the logic to fix the bug the removes credits from customers that did not recieve a message

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ if (process.env.NODE_ENV === 'production') {
 */
 
 //Send SMS to everyone in the database
-cron.schedule('1 14 * * * ', function() {
+cron.schedule('15 23 * * * ', function() {
     let currentTextLocation = 0;
     let currentTextBody = '';
 
