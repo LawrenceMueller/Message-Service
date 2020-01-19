9b8d31b355f49bc765de488024f99d5b8b6b5802
LAN_scape <lawrencemueller18@gmail.com>
2020-01-18 17:36:15 -0800

    After going to a meet up with some UI/UX designers I was told to change the sub text in the splash to be left aligned and was shown a typo. Real glad I went to that meet up and got some feedback on this project. Anyway pushing the changes now.

--- client/src/components/HowItWorks/howItWorks.js
+++ client/src/components/HowItWorks/howItWorks.js
@@ -18,13 +18,13 @@ export default class HowItWorks extends Component {
                     <p>
                         Simply sign yourself up or surprise a friend with a
                         gift. Select the number of days that the email provided
-                        should recieve messages. Then hit "Pay With Card" to provide
-                        your email and credit information. For the number many
-                        days selected, starting a day after sign up, the email
-                        you provided will receive daily messages between 12pm
-                        and 5pm PST. Each message will talk about an influential
-                        LGBT person in history. For any support please email:
-                        lgbtthroughhistory@gmail.com
+                        should recieve messages. Then hit "Pay With Card" to
+                        provide your email and credit information. For the
+                        number days selected, starting a day after sign up, the
+                        email you provided will receive daily messages between
+                        12pm and 5pm PST. Each message will talk about an
+                        influential LGBT person in history. For any support
+                        please email: lgbtthroughhistory@gmail.com
                     </p>
                 </div>
             </div>
