772e08912c57286ba74ef45e376f1ac8c821e32d
LAN_scape <lawrencemueller18@gmail.com>
2020-06-25 19:53:33 -0700

    Error fixed returning message sending time to normal

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === 'production') {
 */
 
 //Send emails to everyone in the database
-cron.schedule('50 19 * * * ', function() {
+cron.schedule('1 14 * * * ', function() {
     let currentTextLocation = 0;
     let currentTextBody = '';
 
